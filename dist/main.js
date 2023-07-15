dc178eab3a380fc5f6d63f802b0d17395652f6e8
ccordoba464 <93160733+ccordoba464@users.noreply.github.com>
2023-07-15 13:27:34 -0400

    Set up DOM layout and declare functions to fetch weather data and process JSON data

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,1 @@
-console.log("test");
+(()=>{let e=document.getElementById("search-input");document.getElementById("submit-button").addEventListener("click",(()=>{e.value})),(async e=>{try{return await fetch("https://api.weatherapi.com/v1/current.json?key=d1bfed572118495ea1d193651231407&q=london",{mode:"cors"})}catch(e){console.log(e)}})()})();
--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log("test");
+let searchInput = document.getElementById("search-input");
+let submitButton = document.getElementById("submit-button");
+
+const fetchWeather = async search => {
+  try {
+    let response = await fetch(
+      `https://api.weatherapi.com/v1/current.json?key=d1bfed572118495ea1d193651231407&q=${search}`,
+      { mode: "cors" }
+    );
+    return response;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const processJSON = async response => {
+  let json = await response.json();
+  console.log(json);
+};
+
+submitButton.addEventListener("click", () => {
+  let search = searchInput.value;
+});
+
+fetchWeather("london");
